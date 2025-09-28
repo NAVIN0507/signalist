377d968ed56f0b3aa213c9ddec3b8a06f30915fa
NAVIN0507 <navinofficial2005@gmail.com>
2025-09-28 20:53:33 +0530

    Completed header and userdrodown

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -11,6 +11,8 @@ import{
 import { useRouter } from 'next/navigation'
 import { Button } from './ui/button'
 import { Avatar, AvatarFallback } from './ui/avatar'
+import { LogOutIcon } from 'lucide-react'
+import NavItems from './NavItems'
 const UserDropdown = () => {
   const router = useRouter();
   const handleSignout  = async()=>{
@@ -24,15 +26,38 @@ const UserDropdown = () => {
       <Avatar className='h-8 w-8'>
         <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>{user.name[0]}</AvatarFallback>
       </Avatar>
+      <div className='hidden md:flex flex-col items-start'>
+        <span className='text-base font-medium text-gray-400'>
+          {user.name}
+        </span>
+      </div>
     </Button>
   </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  <DropdownMenuContent className='text-gray-400'>
+    <DropdownMenuLabel>
+      <div className='flex relative items-center gap-3'>
+        <Button variant={"ghost"} className='flex items-center gap-3 text-gray-400 hover:text-yellow-500'>
+      <Avatar className='h-8 w-8'>
+        <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>{user.name[0]}</AvatarFallback>
+      </Avatar>
+      <div className='flex md:flex flex-col items-start'>
+        <span className='text-base font-medium text-gray-400'>
+          {user.name}
+        </span>
+        <span className='text-sm text-gray-500'>{user.email}</span>
+      </div>
+    </Button>
+      </div>
+    </DropdownMenuLabel>
+    <DropdownMenuSeparator className='bg-gray-600'/>
+    <DropdownMenuItem onClick={handleSignout} className='flex gap-2 text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500'>
+      <LogOutIcon className='w-4 h-4 mr-2 hidden sm:block'/>
+      Logout
+    </DropdownMenuItem>
+        <DropdownMenuSeparator className='block sm:hidden bg-gray-600'/>
+           <nav className='sm:hidden'>
+            <NavItems/>   
+           </nav>
   </DropdownMenuContent>
 </DropdownMenu>   
   )
